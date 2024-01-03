2bed3697ee07824e3e6ef220d7f6d9a1b7955f6f
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-03 09:38:37 -0300

    corrigido problema ao realizar o comentario

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
 import usefetch from "../Login/Hooks/usefetch";
 import { COMMENT_POST } from "../../api";
+import Error from '../Helper/Error'
 
-const PhotoCommentsForm = ({ id }) => {
+const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState("");
   const { request, error } = usefetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    await request(url, options);
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
@@ -20,11 +25,12 @@ const PhotoCommentsForm = ({ id }) => {
         name="comment"
         placeholder="Comente..."
         value={comment}
-        onChange={(target) => setComment(target.value)}
+        onChange={({ target }) => setComment(target.value)}
       />
       <button>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };
