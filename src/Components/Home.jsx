b147fc0ef440e54f4a00c40476797404b4bb1bde
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-05 16:32:08 -0300

    adicionado a animação do loading

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Feed from '../Components/Feed/Feed'
+import Feed from "../Components/Feed/Feed";
+import Loading from "./Helper/Loading";
 
 const Home = () => {
-  return <section className="container mainContainer">
+  return (
+    <section className="container mainContainer">
+      <Loading />
 
-    <Feed/>
-  </section>;
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
