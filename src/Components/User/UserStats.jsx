4ad8ac7ca06f028ed433b8e0ddef463cfca9a02e
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-10 21:45:40 -0300

    adicionado a função de estatisticas

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Head from "../Helper/Head";
+import usefetch from "../Login/Hooks/usefetch";
+import { STATS_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      Estatisticas dados
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, loading, error, request } = usefetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, []);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+       
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  return null;
 };
 
 export default UserStats;
