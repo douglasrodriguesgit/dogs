935f33547afc5606f21446a992da2aae3fab38a4
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-02-06 16:59:34 -0300

    alterado o nome da rota base

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,10 @@ function App() {
           <Header />
           <main className="AppBody">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route
+                path="/https://douglasrodriguesgit.github.io/dogs/"
+                element={<Home />}
+              />
               <Route path="login/*" element={<Login />} />
               <Route
                 path="conta/*"
